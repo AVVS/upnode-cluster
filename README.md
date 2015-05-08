48bcdce946062d8580b79aeecc405d03a362e444
AVVS <v@aminev.me>
2015-05-07 20:26:58 -0700

    Basic connection tests

---
+++ README.md
@@ -0,0 +1,4 @@
+# Upnode cluster
+
+Creates interconnected nodes that share specific resources and make sure that
+there is only 1 example of it in the system
--- test/basic-connection.js
+++ test/basic-connection.js
@@ -4,11 +4,13 @@
 
 var expect = require('expect.js');
 var _ = require('lodash');
+var Redis = require('ioredis');
 
 describe('basic-connection', function () {
 
     var Server = require('../lib/server.js');
     var Peer = require('../lib/peer.js');
+    var Node = require('../lib/node.js');
 
     var serverOpts = {
         port: 7777,
@@ -21,6 +23,66 @@ describe('basic-connection', function () {
 
     it('is able to connect to server', function (done) {
         this.peer = new Peer(_.extend({}, serverOpts), done);
+    });
+
+    it('is able to close server', function (done) {
+        this.server.close(done);
+    });
+
+    it('is able to close peer connection', function (done) {
+        this.peer.close(done);
+    });
+
+    it('able to setup server node', function (done) {
+        this.server = new Node({
+            server: serverOpts,
+            redis: new Redis(),
+            pubsubChannel: '{upnode-cluster}',
+            cluster: 'upnode-cluster-ark'
+        }, done);
+    });
+
+    it('able to setup peer node', function (done) {
+        this.peer = new Node({
+            redis: new Redis(),
+            pubsubChannel: '{upnode-cluster}',
+            cluster: 'upnode-cluster-ark',
+            peer: true
+        }, done);
+    });
+
+    it('some time passes by...', function (done) {
+        setTimeout(done, 500);
+    });
+
+    it('peer must be connected to server', function () {
+        expect(this.server.server._clients).to.have.length(1);
+        expect(_.keys(this.peer._peers)).to.have.length(1);
+        expect(this.peer._peers['localhost:7777']).to.be.an('object');
+    });
+
+    it('launch multi-node', function (done) {
+        this.node = new Node({
+            redis: new Redis(),
+            pubsubChannel: '{upnode-cluster}',
+            cluster: 'upnode-cluster-ark',
+            peer: true,
+            server: {
+                port: 7778,
+                host: 'localhost'
+            }
+        }, done);
+    });
+
+    it('some time passes by', function (done) {
+        setTimeout(done, 500);
+    });
+
+    it('expect multi-node to be connected to other servers', function () {
+        expect(_.keys(this.node._peers)).to.have.length(1);
+        expect(this.server.server._clients).to.have.length(2);
+        expect(this.node.server._clients).to.have.length(1);
+        expect(_.keys(this.peer._peers)).to.have.length(2);
     });
 
 });
